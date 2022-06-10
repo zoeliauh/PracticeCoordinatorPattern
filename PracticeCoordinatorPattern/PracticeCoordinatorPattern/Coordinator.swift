766651bc6b44d7d853efd25a86b950b645656b1b
Zoe Liauh <woanjwuliauh@gmail.com>
2022-06-10 13:45:47 +0800

    feat: delete storyboard and prepare protocol coordinator

---
+++ PracticeCoordinatorPattern/PracticeCoordinatorPattern/Coordinator.swift
@@ -0,0 +1,28 @@
+//
+//  Coordinator.swift
+//  PracticeCoordinatorPattern
+//
+//  Created by woanjwu liauh on 2022/6/10.
+//
+
+import Foundation
+import UIKit
+
+enum Event {
+    case buttonTapped
+}
+
+protocol Coordinator {
+    var navifationController: UINavigationController? { get set }
+    
+    // 其他 VC 告訴 coordinator 有事發生，需要處理
+    func eventOccured(with type: Event)
+    
+    // 當 app 執行時，coordinator 需要被告知開始執行
+    func start()
+}
+
+// 所有的 VC 都需要有個中繼的 coordinator
+protocol Coordinatiog {
+    var coordinator: Coordinator? { get set }
+}
